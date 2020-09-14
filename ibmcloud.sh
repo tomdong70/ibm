3cf09bcb6190b65f447473d7fe81a68226eb6ad8
tomdong70 <43061674+tomdong70@users.noreply.github.com>
2020-09-14 19:27:11 +0800

    Update ibmcloud.sh

--- ibmcloud.sh
+++ ibmcloud.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-
-	appname=redruddy0913
-
+read -p "请输入应用程序名称:" appname
+read -p "请设置你的容器内存大小(默认256):" ramsize
+if [ -z "$ramsize" ];then
 	ramsize=256
-
-
+fi
+rm -rf cloudfoundry
+mkdir cloudfoundry
+cd cloudfoundry
 
 echo '<!DOCTYPE html> '>>index.php
 echo '<html> '>>index.php
@@ -15,6 +17,7 @@ echo '?> '>>index.php
 echo '<body>'>>index.php
 echo '</html>'>>index.php
 
+wget https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip -d v2ray1 v2ray-linux-64.zip
 cd v2ray1
 chmod 777 *
@@ -23,7 +26,6 @@ rm -rf v2ray-linux-64.zip
 mv $HOME/cloudfoundry/v2ray1/v2ray $HOME/cloudfoundry/v2ray
 mv $HOME/cloudfoundry/v2ray1/v2ctl $HOME/cloudfoundry/v2ctl
 rm -rf $HOME/cloudfoundry/v2ray1
-
 uuid=`cat /proc/sys/kernel/random/uuid`
 path=`echo $uuid | cut -f1 -d'-'`
 echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/cloudfoundry/config.json
