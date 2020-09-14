dfde14a85f7a9b8339b990abf7dd40be9ffe7f4b
tomdong70 <43061674+tomdong70@users.noreply.github.com>
2020-09-14 19:31:56 +0800

    Update ibmcloud.sh

--- ibmcloud.sh
+++ ibmcloud.sh
@@ -4,9 +4,9 @@ read -p "请设置你的容器内存大小(默认256):" ramsize
 if [ -z "$ramsize" ];then
 	ramsize=256
 fi
-rm -rf cloudfoundry
-mkdir cloudfoundry
-cd cloudfoundry
+rm -rf tomdong
+mkdir tomdong
+cd tomdong
 
 echo '<!DOCTYPE html> '>>index.php
 echo '<html> '>>index.php
@@ -23,12 +23,12 @@ cd v2ray1
 chmod 777 *
 cd ..
 rm -rf v2ray-linux-64.zip
-mv $HOME/cloudfoundry/v2ray1/v2ray $HOME/cloudfoundry/v2ray
-mv $HOME/cloudfoundry/v2ray1/v2ctl $HOME/cloudfoundry/v2ctl
-rm -rf $HOME/cloudfoundry/v2ray1
+mv $HOME/tomdong/v2ray1/v2ray $HOME/tomdong/v2ray
+mv $HOME/tomdong/v2ray1/v2ctl $HOME/tomdong/v2ctl
+rm -rf $HOME/tomdong/v2ray1
 uuid=`cat /proc/sys/kernel/random/uuid`
 path=`echo $uuid | cut -f1 -d'-'`
-echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/cloudfoundry/config.json
+echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/tomdong/config.json
 echo 'applications:'>>manifest.yml
 echo '- path: .'>>manifest.yml
 echo '  command: '/app/htdocs/v2ray'' >>manifest.yml
