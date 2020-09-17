9d43fc2d43a777ab2bc87243d1dd1c59bd423b01
tomdong70 <43061674+tomdong70@users.noreply.github.com>
2020-09-17 10:24:13 +0800

    Update ibmcloud-0.sh

--- ibmcloud-0.sh
+++ ibmcloud-0.sh
@@ -1,31 +1,30 @@
 #!/bin/sh
-read -p "请输入应用程序名称:" appname
-read -p "请设置你的容器内存大小(默认256):" ramsize
-if [ -z "$ramsize" ];then
-	ramsize=256
-fi
 
-appname=redruddy0913
+	ramsize=256
+
+
+	appname=redruddy0913
 
 cd tomdong
 
 wget -N https://raw.githubusercontent.com/tomdong70/ibm/master/ws.sh
 
-rm -rf manifest.yml
+rm -rf $HOME/tomdong/ws.zip
+wget -N https://raw.githubusercontent.com/tomdong70/ibm/master/config.json
+zip ws.zip config.json
+rm -rf $HOME/tomdong/config.json
 
+rm -rf manifest.yml
 echo 'applications:'>>manifest.yml
 echo '- path: .'>>manifest.yml
 echo '  command: '/app/htdocs/ws.sh'' >>manifest.yml
 echo '  name: '$appname''>>manifest.yml
 echo '  random-route: true'>>manifest.yml
 echo '  memory: '$ramsize'M'>>manifest.yml
+
 ibmcloud target --cf
 ibmcloud cf push
+
 domain=`ibmcloud cf app $appname | grep routes | cut -f2 -d':' | sed 's/ //g'`
-vmess=`echo '{"add":"'$domain'","aid":"64","host":"","id":"'$uuid'","net":"ws","path":"/'$path'","port":"443","ps":"IBM_Cloud","tls":"tls","type":"none","v":"2"}' | base64 -w 0`
 cd ..
-echo 容器已经成功启动
-echo 地址: $domain
-echo UUID: $uuid
-echo path: /$path
-echo vmess://$vmess
+echo OK
