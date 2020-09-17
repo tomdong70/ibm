0d9d50868d65968cd751edcb1da3d8f72b9a607b
tomdong70 <43061674+tomdong70@users.noreply.github.com>
2020-09-17 10:54:28 +0800

    Update ws.sh

--- ws.sh
+++ ws.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-wget -N https://raw.githubusercontent.com/tomdong70/ibm/master/config.json
+#wget -N https://raw.githubusercontent.com/tomdong70/ibm/master/config.json
 
 unzip -q ws.zip
 chmod 777 *
-v2ray -config config.json
+ws -config config.json
